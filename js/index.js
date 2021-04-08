9a45a03d53caea5c48e065e97cd2c7f671736999
Jeff <macookpro@MACOOKs-MacBook-Pro.local>
2021-04-07 21:40:29 -0400

    Not yet done, finished HTML update

--- js/index.js
+++ js/index.js
@@ -40,3 +40,77 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//NAVIGATION START
+const navAnch = document.querySelectorAll('nav a')
+
+navAnch.forEach((anchor, index) => {
+  anchor.textContent = siteContent ['nav']['nav-item-' + (index + 1)]
+})
+//Alternative
+// avAnch[0].textContent = siteContent['nav']['nav-item-1']
+// avAnch[1].textContent = siteContent['nav']['nav-item-2']
+// avAnch[2].textContent = siteContent['nav']['nav-item-3']
+// avAnch[3].textContent = siteContent['nav']['nav-item-4']
+// avAnch[4].textContent = siteContent['nav']['nav-item-5']
+// avAnch[5].textContent = siteContent['nav']['nav-item-6']
+//NAVIGATION END
+
+//HEADER IMAGE
+let headImg = document.querySelector("#cta-img");
+headImg.setAttribute('src', siteContent["cta"]["img-src"])
+
+//CTA-H1/HEADER ETC
+let headOne = document.querySelector("h1")
+let headOneSplit = siteContent["cta"]["h1"].split(' ').join('<br>')
+headOne.innerHTML = headOneSplit
+
+//Button
+
+let button = document.querySelector(".cta-text button")
+button.textContent = siteContent["cta"]["button"]
+
+//Main-Content
+let mainH4 = document.querySelectorAll('.text-content h4')
+let mainPar = document.querySelectorAll('.text-content p')
+
+mainH4[0].textContent = siteContent['main-content']['features-h4']
+mainPar[0].textContent = siteContent['main-content']['features-content']
+
+mainH4[1].textContent = siteContent['main-content']['about-h4']
+mainPar[1].textContent = siteContent['main-content']['about-content']
+
+//Middle-Image
+let midImg = document.querySelector(".middle-img")
+midImg.setAttribute('src', siteContent['main-content']['middle-img-src'])
+
+
+//Main-Content-Contined
+mainH4[2].textContent = siteContent['main-content']['services-h4']
+mainPar[2].textContent = siteContent['main-content']['services-content']
+
+mainH4[3].textContent = siteContent['main-content']['product-h4']
+mainPar[3].textContent = siteContent['main-content']['product-content']
+
+mainH4[4].textContent = siteContent['main-content']['vision-h4']
+mainPar[4].textContent = siteContent['main-content']['vision-content']
+
+//Main-Content-END
+
+//Contact
+let contH4 = document.querySelector('.contact h4')
+let contPar = document.querySelectorAll('.contact p')
+
+contH4.textContent = siteContent['contact']['contact-h4']
+
+contPar[0].textContent = siteContent['contact']['address']
+
+contPar[1].textContent = siteContent['contact']['phone']
+
+contPar[2].textContent = siteContent['contact']['email']
+
+//Footer 
+let footPar = document.querySelector('footer p')
+footPar.textContent = siteContent['footer']['copyright']
+
+
